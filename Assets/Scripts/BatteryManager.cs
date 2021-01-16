474e7ca5d46fd5d9ecf81daf8f420c192a917fc1
warmoverdrive <sweetsthebear@gmail.com>
2021-01-16 09:40:53 -0800

    # Flashlight 2 - Set up battery indicator with PhoneOS timer

--- Assets/Scripts/BatteryManager.cs
+++ Assets/Scripts/BatteryManager.cs
@@ -1,18 +1,125 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BatteryManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	// Config Params ----------------------
+	[SerializeField]
+	Image lowImage, midLowImage, midImage, midHighImage, highImage;
+	[SerializeField]
+	Color normalBatteryColor = Color.white;
+	[SerializeField]
+	Color lowBatteryColor = Color.red;
+	[SerializeField]
+	float lowBattFlickerSpeed = 0.5f;
+	[SerializeField]
+	float deadFlickerSpeed = 0.25f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public enum BatteryState { high, midHigh, mid, midLow, low, dead };
+
+	// Internal Vars ----------------------
+	Image thisImage;
+	BatteryState batteryState = BatteryState.high;
+	bool flickerOn = true;
+	float currentFlickerTime = 0f;
+
+	private void Start()
+	{
+		thisImage = GetComponent<Image>();
+	}
+
+	void Update()
+	{
+		if (batteryState == BatteryState.low || batteryState == BatteryState.dead)
+			LowPowerCycle();
+	}
+
+	public void ChangeBatteryState(BatteryState state)
+	{
+		if (batteryState != state)
+		{
+			switch (state)
+			{
+				case BatteryState.high:
+					thisImage.enabled = true;
+					highImage.gameObject.SetActive(true);
+					midHighImage.gameObject.SetActive(true);
+					midImage.gameObject.SetActive(true);
+					midLowImage.gameObject.SetActive(true);
+					lowImage.gameObject.SetActive(true);
+					thisImage.color = normalBatteryColor;
+					lowImage.color = normalBatteryColor;
+					break;
+				case BatteryState.midHigh:
+					thisImage.enabled = true;
+					highImage.gameObject.SetActive(false);
+					midHighImage.gameObject.SetActive(true);
+					midImage.gameObject.SetActive(true);
+					midLowImage.gameObject.SetActive(true);
+					lowImage.gameObject.SetActive(true);
+					thisImage.color = normalBatteryColor;
+					lowImage.color = normalBatteryColor;
+					break;
+				case BatteryState.mid:
+					thisImage.enabled = true;
+					highImage.gameObject.SetActive(false);
+					midHighImage.gameObject.SetActive(false);
+					midImage.gameObject.SetActive(true);
+					midLowImage.gameObject.SetActive(true);
+					lowImage.gameObject.SetActive(true);
+					thisImage.color = normalBatteryColor;
+					lowImage.color = normalBatteryColor;
+					break;
+				case BatteryState.midLow:
+					thisImage.enabled = true;
+					highImage.gameObject.SetActive(false);
+					midHighImage.gameObject.SetActive(false);
+					midImage.gameObject.SetActive(false);
+					midLowImage.gameObject.SetActive(true);
+					lowImage.gameObject.SetActive(true);
+					thisImage.color = normalBatteryColor;
+					lowImage.color = normalBatteryColor;
+					break;
+				case BatteryState.low:
+					thisImage.enabled = true;
+					highImage.gameObject.SetActive(false);
+					midHighImage.gameObject.SetActive(false);
+					midImage.gameObject.SetActive(false);
+					midLowImage.gameObject.SetActive(false);
+					lowImage.gameObject.SetActive(true);
+					thisImage.color = lowBatteryColor;
+					lowImage.color = lowBatteryColor;
+					break;
+				case BatteryState.dead:
+					thisImage.enabled = true;
+					highImage.gameObject.SetActive(false);
+					midHighImage.gameObject.SetActive(false);
+					midImage.gameObject.SetActive(false);
+					midLowImage.gameObject.SetActive(false);
+					lowImage.gameObject.SetActive(false);
+					thisImage.color = lowBatteryColor;
+					lowImage.color = lowBatteryColor;
+					break;
+			}
+			batteryState = state;
+		}
+	}
+
+	private void LowPowerCycle()
+	{
+		if (currentFlickerTime > lowBattFlickerSpeed && batteryState == BatteryState.low)
+		{
+			flickerOn = !flickerOn;
+			lowImage.gameObject.SetActive(!lowImage.gameObject.activeSelf);
+			currentFlickerTime = 0;
+		}
+		else if (currentFlickerTime > deadFlickerSpeed && batteryState == BatteryState.dead)
+		{
+			flickerOn = !flickerOn;
+			thisImage.enabled = !thisImage.enabled;
+			currentFlickerTime = 0;
+		}
+		currentFlickerTime += Time.deltaTime;
+	}
 }
--- Assets/Scripts/PhoneOS.cs
+++ Assets/Scripts/PhoneOS.cs
@@ -4,9 +4,48 @@ using UnityEngine.UI;
 public class PhoneOS : MonoBehaviour
 {
 	// Config Params ---------------
+	[Header("OS Properties")]
+	[SerializeField]
+	float batteryMax = 100f;
+	[SerializeField]
+	float batteryDrainRate = 1f;
 	[Header("OS Screen Objects")]
 	[SerializeField]
 	Image offScreen;
+
+	public float currentBattery;
+
+	bool isDead = false;
+
+	BatteryManager batteryManager;
+
+	private void Start()
+	{
+		batteryManager = GetComponentInChildren<BatteryManager>();
+		currentBattery = batteryMax;
+	}
+
+	private void Update()
+	{
+		currentBattery -= batteryDrainRate * Time.deltaTime;
+		currentBattery = Mathf.Clamp(currentBattery, 0, batteryMax);
+
+		if (currentBattery <= 0)
+		{
+			batteryManager.ChangeBatteryState(BatteryManager.BatteryState.dead);
+			isDead = true;
+		}
+		else if (currentBattery == batteryMax)
+			batteryManager.ChangeBatteryState(BatteryManager.BatteryState.high);
+		else if (currentBattery < batteryMax / 5)
+			batteryManager.ChangeBatteryState(BatteryManager.BatteryState.low);
+		else if (currentBattery < (batteryMax / 5) * 2)
+			batteryManager.ChangeBatteryState(BatteryManager.BatteryState.midLow);
+		else if (currentBattery < (batteryMax / 5) * 3)
+			batteryManager.ChangeBatteryState(BatteryManager.BatteryState.mid);
+		else if (currentBattery < (batteryMax / 5) * 4)
+			batteryManager.ChangeBatteryState(BatteryManager.BatteryState.midHigh);
+	}
 
 	public void ToggleLight()
 	{
